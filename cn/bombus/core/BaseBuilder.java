f7bcde56003579f0c21fb0387234981884ca8619
wangxiaofei <wangxiaofei@corp.netease.com>
2017-02-13 19:34:09 +0800

    格式化BaseBuilder

--- cn/bombus/core/BaseBuilder.java
+++ cn/bombus/core/BaseBuilder.java
@@ -6,95 +6,136 @@ import cn.bombus.core.sql.result.ResultSetType;
 import cn.bombus.core.sql.type.JdbcType;
 import cn.bombus.core.sql.type.TypeAliasRegistry;
 
-public abstract class BaseBuilder {
+public abstract class BaseBuilder
+{
+	/**
+	 * 最基本的一个类
+	 */
 	protected final Configuration configuration;
 	protected final TypeAliasRegistry typeAliasRegistry;
-	// protected final TypeHandlerRegistry typeHandlerRegistry;
 
-	public BaseBuilder(Configuration configuration) {
+	public BaseBuilder(Configuration configuration)
+	{
 		this.configuration = configuration;
 		this.typeAliasRegistry = this.configuration.getTypeAliasRegistry();
-		// this.typeHandlerRegistry =
-		// this.configuration.getTypeHandlerRegistry();
 	}
 
-	public Configuration getConfiguration() {
+	public Configuration getConfiguration()
+	{
 		return configuration;
 	}
 
-	protected String stringValueOf(String value, String defaultValue) {
+	protected String stringValueOf(String value, String defaultValue)
+	{
 		return value == null ? defaultValue : value;
 	}
 
-	protected Boolean booleanValueOf(String value, Boolean defaultValue) {
+	protected Boolean booleanValueOf(String value, Boolean defaultValue)
+	{
 		return value == null ? defaultValue : Boolean.valueOf(value);
 	}
 
-	protected Integer integerValueOf(String value, Integer defaultValue) {
+	protected Integer integerValueOf(String value, Integer defaultValue)
+	{
 		return value == null ? defaultValue : Integer.valueOf(value);
 	}
 
-	protected ResultSetType resolveResultSetType(String alias) {
+	protected ResultSetType resolveResultSetType(String alias)
+	{
 		if (alias == null)
+		{
 			return null;
-		try {
+		}
+		try
+		{
 			return ResultSetType.valueOf(alias);
-		} catch (IllegalArgumentException e) {
+		}
+		catch (IllegalArgumentException e)
+		{
 			throw new BuilderException("Error resolving ResultSetType. Cause: " + e, e);
 		}
 	}
 
-	protected Class<?> resolveClass(String alias) {
+	protected Class<?> resolveClass(String alias)
+	{
 		if (alias == null)
+		{
 			return null;
-		try {
+		}
+		try
+		{
 			return resolveAlias(alias);
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			throw new BuilderException("Error resolving class . Cause: " + e, e);
 		}
 	}
 
-	protected Object resolveInstance(String alias) {
+	protected Object resolveInstance(String alias)
+	{
 		if (alias == null)
 			return null;
-		try {
+		try
+		{
 			Class<?> type = resolveClass(alias);
 			return type.newInstance();
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			throw new BuilderException("Error instantiating class. Cause: " + e, e);
 		}
 	}
 
-	protected Object resolveInstance(Class<?> type) {
+	protected Object resolveInstance(Class<?> type)
+	{
 		if (type == null)
+		{
 			return null;
-		try {
+		}
+		try
+		{
 			return type.newInstance();
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			throw new BuilderException("Error instantiating class. Cause: " + e, e);
 		}
 	}
 
-	protected Class<?> resolveAlias(String alias) {
+	protected Class<?> resolveAlias(String alias)
+	{
 		return typeAliasRegistry.resolveAlias(alias);
 	}
 
-	protected JdbcType resolveJdbcType(String alias) {
+	protected JdbcType resolveJdbcType(String alias)
+	{
 		if (alias == null)
+		{
 			return null;
-		try {
+		}
+		try
+		{
 			return JdbcType.valueOf(alias);
-		} catch (IllegalArgumentException e) {
+		}
+		catch (IllegalArgumentException e)
+		{
 			throw new BuilderException("Error resolving JdbcType. Cause: " + e, e);
 		}
 	}
 
-	protected ParameterMode resolveParameterMode(String alias) {
+	protected ParameterMode resolveParameterMode(String alias)
+	{
 		if (alias == null)
+		{
 			return null;
-		try {
+		}
+		try
+		{
 			return ParameterMode.valueOf(alias);
-		} catch (IllegalArgumentException e) {
+		}
+		catch (IllegalArgumentException e)
+		{
 			throw new BuilderException("Error resolving ParameterMode. Cause: " + e, e);
 		}
 	}
