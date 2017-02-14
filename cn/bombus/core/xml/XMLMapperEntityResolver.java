d079165cb06d8b0b21b93a8d2e245795ae56aafe
wangxiaofei <wangxiaofei@corp.netease.com>
2017-02-14 17:58:25 +0800

    代码格式化

--- cn/bombus/core/xml/XMLMapperEntityResolver.java
+++ cn/bombus/core/xml/XMLMapperEntityResolver.java
@@ -12,49 +12,59 @@ import org.xml.sax.SAXException;
 
 import cn.bombus.core.Resources;
 
-public class XMLMapperEntityResolver implements EntityResolver {
+public class XMLMapperEntityResolver implements EntityResolver
+{
 
 	private static final Map<String, String> doctypeMap = new HashMap<String, String>();
-
 	private static final String MYBATIS_MAPPER_DOCTYPE = "-//bombus.cn//DTD Mapper 3.0//EN".toUpperCase(Locale.ENGLISH);
 	private static final String MYBATIS_MAPPER_URL = "http://bombus.cn/dtd/mapper.dtd".toUpperCase(Locale.ENGLISH);
-
 	private static final String IBATIS_MAPPER_DTD = "cn/bombus/core/xml/dtd/mapper.dtd";
-
-	static {
-
+	static
+	{
 		doctypeMap.put(MYBATIS_MAPPER_URL, IBATIS_MAPPER_DTD);
 		doctypeMap.put(MYBATIS_MAPPER_DOCTYPE, IBATIS_MAPPER_DTD);
 	}
 
-	public InputSource resolveEntity(String publicId, String systemId) throws SAXException {
-
+	public InputSource resolveEntity(String publicId, String systemId) throws SAXException
+	{
 		if (publicId != null)
+		{
 			publicId = publicId.toUpperCase(Locale.ENGLISH);
+		}
 		if (systemId != null)
+		{
 			systemId = systemId.toUpperCase(Locale.ENGLISH);
-
+		}
 		InputSource source = null;
-		try {
+		try
+		{
 			String path = doctypeMap.get(publicId);
 			source = getInputSource(path, source);
-			if (source == null) {
+			if (source == null)
+			{
 				path = doctypeMap.get(systemId);
 				source = getInputSource(path, source);
 			}
-		} catch (Exception e) {
+		}
+		catch (Exception e)
+		{
 			throw new SAXException(e.toString());
 		}
 		return source;
 	}
 
-	private InputSource getInputSource(String path, InputSource source) {
-		if (path != null) {
+	private InputSource getInputSource(String path, InputSource source)
+	{
+		if (path != null)
+		{
 			InputStream in;
-			try {
+			try
+			{
 				in = Resources.getResourceAsStream(path);
 				source = new InputSource(in);
-			} catch (IOException e) {
+			}
+			catch (IOException e)
+			{
 				// ignore, null is ok
 			}
 		}
